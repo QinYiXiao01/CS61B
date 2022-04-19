0e4e57ea5d117deab3b3692bd2d9e67955057fbc
Qin <danchin01@163.com>
2022-04-19 14:59:24 +0800

    完成hw2

--- hw2/hw2/Percolation.java
+++ hw2/hw2/Percolation.java
@@ -1,13 +1,14 @@
 package hw2;
 
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
+import org.junit.Test;
 
 import java.util.HashSet;
 
 public class Percolation {
     private WeightedQuickUnionUF percSet;
     private int lineLength;
-    private HashSet openSite;
+    private HashSet<Integer> openSite;
 
     /**
      * create N-by-N grid, with all sites initially blocked
@@ -21,12 +22,27 @@ public class Percolation {
     }
 
     /**
-     * open the site (row, col) if it is not open already
+     * 将相邻的格子连通
+     */
+    public void union(int newlyOpen) {
+        for (int site : openSite) {
+            int sub = Math.abs(site - newlyOpen);
+            // 如果现有的格子序号和新打开的格子序号相差1格或1行，就连通
+            if (sub == 1 || sub == lineLength) {
+                percSet.union(site, newlyOpen);
+            }
+        }
+    }
+
+    /**
+     * 将打开的格子加到set里
      * @param row
      * @param col
      */
     public void open(int row, int col){
-        openSite.add(locate(row, col));
+        int newlyOpen = locate(row, col);
+        openSite.add(newlyOpen);
+        union(newlyOpen);
     }
 
     /**
@@ -47,9 +63,55 @@ public class Percolation {
     public boolean isOpen(int row, int col){
         return openSite.contains(locate(row, col));
     }
-    public boolean isFull(int row, int col){}  // is the site (row, col) full?
-    public int numberOfOpenSites(){}           // number of open sites
-    public boolean percolates(){}              // does the system percolate?
+
+    /**
+     * 从上面通下来，相当于灌水，联通的空格子都是full
+     * @param row
+     * @param col
+     * @return
+     */
+    public boolean isFull(int row, int col){
+        // 如果这个格子是和第一行联通的，就说明他满了
+        for (int site : openSite) {
+            if (site < lineLength) {
+                return percSet.connected(locate(row, col), site);
+            }
+        }
+        return false;
+    }
+
+    public boolean isFull(int targetSite){
+        // 如果这个格子是和第一行联通的，就说明他满了
+        for (int site : openSite) {
+            if (site < lineLength) {
+                return percSet.connected(targetSite, site);
+            }
+        }
+        return false;
+    }
+
+    /**
+     * 返回open格子的个数
+     * @return
+     */
+    public int numberOfOpenSites(){
+        return openSite.size();
+    }
+
+    /**
+     * does the system percolate?
+     * @return
+     */
+    public boolean percolates(){
+        // 如果最后一行的open格子满了，就说明联通了
+        for (int site : openSite) {
+            if (site >= lineLength * (lineLength - 1) && site <= lineLength * lineLength) {
+                return isFull(site);
+            }
+        }
+        return false;
+    }
+
     public static void main(String[] args){}   // use for unit testing (not required)
 
 }
--- hw2/hw2/PercolationStats.java
+++ hw2/hw2/PercolationStats.java
@@ -1,5 +1,67 @@
 package hw2;
 
-public class PercolationStats {
+import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.StdStats;
 
+public class PercolationStats {
+    private double[] fractions;
+    private int totalTime;
+
+    /**
+     * perform T independent experiments on an N-by-N grid
+     * @param N
+     * @param T
+     * @param pf
+     */
+    public PercolationStats(int N, int T, PercolationFactory pf) {
+        // T可以理解为进行几次试验
+        if (N <= 0 || T <= 0) {
+            throw new IllegalArgumentException("N & T 应该大于0");
+        }
+        this.totalTime = T;
+        for (int i = 0; i < T; i++) {
+            Percolation perc = pf.make(N);
+            int row = StdRandom.uniform(N);
+            int col = StdRandom.uniform(N);
+            while (!perc.percolates()) {
+                if (!perc.isOpen(row, col)) {
+                    perc.open(row, col);
+                }
+            }
+            // fraction为open的格子所占比例
+            double fraction = perc.numberOfOpenSites() / N * N;
+            fractions[i] = fraction;
+        }
+
+    }
+
+    /**
+     * sample mean of percolation threshold
+     * @return
+     */
+    public double mean() {
+        return StdStats.mean(fractions);
+    }
+
+    /**
+     * sample standard deviation of percolation threshold
+     * @return
+     */
+    public double stddev() {
+        return StdStats.stddev(fractions);
+    }
+
+    // low endpoint of 95% confidence interval
+    public double confidenceLow() {
+        double mu = mean();
+        double sigma = stddev();
+        return mu - (1.96 * sigma / Math.sqrt(totalTime));
+    }
+
+    // high endpoint of 95% confidence interval
+    public double confidenceHigh() {
+        double mu = mean();
+        double sigma = stddev();
+        return mu + (1.96 * sigma / Math.sqrt(totalTime));
+    }
 }

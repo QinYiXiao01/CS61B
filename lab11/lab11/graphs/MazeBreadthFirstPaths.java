644c231ba3ed786658bd9ade2e4ae47dd62bf027
Qin <danchin01@163.com>
2022-05-07 16:37:56 +0800

    完成BFS

--- lab11/lab11/graphs/MazeBreadthFirstPaths.java
+++ lab11/lab11/graphs/MazeBreadthFirstPaths.java
@@ -1,5 +1,7 @@
 package lab11.graphs;
 
+import edu.princeton.cs.algs4.Queue;
+
 /**
  *  @author Josh Hug
  */
@@ -9,21 +11,50 @@ public class MazeBreadthFirstPaths extends MazeExplorer {
     public int[] edgeTo;
     public boolean[] marked;
     */
+    private int s; // source
+    private int t; // target
+    private Maze maze;
+
 
     public MazeBreadthFirstPaths(Maze m, int sourceX, int sourceY, int targetX, int targetY) {
         super(m);
-        // Add more variables here!
+        maze = m;
+        s = maze.xyTo1D(sourceX, sourceY); // 返回空间坐标
+        t = maze.xyTo1D(targetX, targetY);
+        distTo[s] = 0;
+        edgeTo[s] = s;
     }
 
     /** Conducts a breadth first search of the maze starting at the source. */
     private void bfs() {
         // TODO: Your code here. Don't forget to update distTo, edgeTo, and marked, as well as call announce()
+        Queue<Integer> queue = new Queue<Integer>();
+        queue.enqueue(s); // 队列存储的是当前vertex的所有邻居
+        marked[s] = true;
+        announce();
+
+        while(!queue.isEmpty()) {
+            int v = queue.dequeue();
+            if (v == t) {
+                return;
+            }
+
+            for (int w : maze.adj(v)) {
+                if (!marked[w]) {
+                    queue.enqueue(w);
+                    edgeTo[w] = v;
+                    marked[w] = true;
+                    distTo[w] = distTo[v] + 1;
+                    announce();
+                }
+            }
+        }
     }
 
 
     @Override
     public void solve() {
-        // bfs();
+        bfs();
     }
 }
 
